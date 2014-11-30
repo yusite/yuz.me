8b1d9c160382ef80e5d24b64394fbc500f5c5f10
Zach YU <git@yuz.me>
2014-11-30 21:51:53 +0100

    now foodlist.js can output foodlist

--- javascript/foodlist.js
+++ javascript/foodlist.js
@@ -9,10 +9,30 @@ request.open('GET', 'https://script.google.com/macros/s/AKfycbxRXGEN3_W6kG-WKyua
 request.onreadystatechange = function() {
     if ((request.readyState === 4) && (request.status === 200)) {
         var items = JSON.parse(request.responseText);
-        var output = '<ul>';
-        output += '</ul>';
-        console.log(items);
+        var foodlist = createOutput(items);
+        document.getElementById('foodlist').innerHTML = foodlist;
     }
 };
 
 request.send();
+
+var createOutput = function( jsonobject ) {
+    var output = '';
+    for (var i in jsonobject) {
+        output += '<h3>';
+        output += i;
+        output += '</h3>';
+        output += '<ul>';
+        var names = jsonobject[i].names;
+        var numbers = jsonobject[i].numbers;
+        for (var j = 0; j < names.length; j++) {
+            output += '<li>';
+            output += names[j];
+            output += '：';
+            output += numbers[j];
+            output += '</li>';
+        }
+        output += '</ul>';
+    }
+    return output;
+};
