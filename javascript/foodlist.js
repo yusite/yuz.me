d5dac8bdd39eeed3d341feaa0c2e8fa7dd49c79f
Zach YU <git@yuz.me>
2014-12-01 12:14:37 +0100

    use jsonp instead of json

--- javascript/foodlist.js
+++ javascript/foodlist.js
@@ -1,57 +1,48 @@
-var request;
-if (window.XMLHttpRequest) {
-    request = new XMLHttpRequest();
-} else {
-    request = new ActiveXObject("Microsoft.XMLHttp");
-}
+(function() {
+    var google = "https://script.google.com/macros/s/AKfycbxRXGEN3_W6kG-WKyuaYVYmHF4W4LSJ46Z7cjR3DYisl4tH8GYG/exec";
+    var Url = google + "?prefix=?";
 
-request.open('GET', 'https://script.google.com/macros/s/AKfycbxRXGEN3_W6kG-WKyuaYVYmHF4W4LSJ46Z7cjR3DYisl4tH8GYG/exec');
-
-request.onreadystatechange = function() {
-    if ((request.readyState === 4) && (request.status === 200)) {
-        var items = JSON.parse(request.responseText);
-        var foodlist = createOutput(items);
+    $.getJSON(Url).done(function(data) {
+        var foodlist = createOutput(data);
         document.getElementById('foodlist').innerHTML = foodlist;
-    }
-};
+    });
 
-request.send();
-
-var createOutput = function( jsonobject ) {
-    var output = '';
-    for (var i in jsonobject) {
-        output += '<h3>';
-        output += i;
-        output += '</h3>';
-        output += '<ul>';
-        var names = jsonobject[i].names;
-        var numbers = jsonobject[i].numbers;
-        var buyDates = jsonobject[i].buyDates;
-        var eatDates = jsonobject[i].eatDates;
-        for (var j = 0; j < names.length; j++) {
-            output += '<li>';
-            output += names[j];
-            output += '：';
-            output += numbers[j];
-            if (eatDates[j] === 0) {
-                output += '，刚刚吃过';
-            } else if (eatDates[j] != "N/A") {
-                output += '，';
-                output += eatDates[j];
-                output += ' 天前吃过';
-            } else {
-                output += '，已多天没吃过';
+    var createOutput = function( jsonobject ) {
+        var output = '';
+        for (var i in jsonobject) {
+            output += '<h3>';
+            output += i;
+            output += '</h3>';
+            output += '<ul>';
+            var names = jsonobject[i].names;
+            var numbers = jsonobject[i].numbers;
+            var buyDates = jsonobject[i].buyDates;
+            var eatDates = jsonobject[i].eatDates;
+            for (var j = 0; j < names.length; j++) {
+                output += '<li>';
+                output += names[j];
+                output += '：';
+                output += numbers[j];
+                if (eatDates[j] === 0) {
+                    output += '，刚刚吃过';
+                } else if (eatDates[j] != "N/A") {
+                    output += '，';
+                    output += eatDates[j];
+                    output += ' 天前吃过';
+                } else {
+                    output += '，已多天没吃过';
+                }
+                if (buyDates[j] === 0) {
+                    output += '，刚刚购买';
+                } else if (buyDates[j] != "N/A") {
+                    output += '，上次购买：';
+                    output += buyDates[j];
+                    output += ' 天前';
+                }
+                output += '</li>';
             }
-            if (buyDates[j] === 0) {
-                output += '，刚刚购买';
-            } else if (buyDates[j] != "N/A") {
-                output += '，上次购买：';
-                output += buyDates[j];
-                output += ' 天前';
-            }
-            output += '</li>';
+            output += '</ul>';
         }
-        output += '</ul>';
-    }
-    return output;
-};
+        return output;
+    };
+})();
