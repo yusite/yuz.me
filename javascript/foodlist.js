61230ec3e98952d710ac90e2bf9f6d1444654676
Zach YU <git@yuz.me>
2014-12-01 03:10:00 +0100

    try to fix bugs

--- javascript/foodlist.js
+++ javascript/foodlist.js
@@ -5,7 +5,8 @@ if (window.XMLHttpRequest) {
     request = new ActiveXObject("Microsoft.XMLHttp");
 }
 
-request.open('GET', 'https://script.google.com/macros/s/AKfycbxRXGEN3_W6kG-WKyuaYVYmHF4W4LSJ46Z7cjR3DYisl4tH8GYG/exec');
+//request.open('GET', 'https://script.google.com/macros/s/AKfycbxRXGEN3_W6kG-WKyuaYVYmHF4W4LSJ46Z7cjR3DYisl4tH8GYG/exec');
+request.open('GET', 'https://script.googleusercontent.com/macros/echo?user_content_key=vK5OlJDVoUpvRhmoiizlI-7vFCUEhHE_2andcRX-HJRh88qwphMpXCkrR4OFNfuEpcirHb-LU7qj390WlKgmdl5OtZrn4F3Km5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnFzJH6wY6_5KwjCq16nyjaa8Qdhsm_r-_gKab0w4nnJ-CcnK9VIh7xqvx3Cqvg8qv4rxIClpAyco&lib=M_U9PJHTkRzmt9VY2kX-rspzzFJIXVw6j');
 request.onreadystatechange = function() {
     if ((request.readyState === 4) && (request.status === 200)) {
         var items = JSON.parse(request.responseText);
