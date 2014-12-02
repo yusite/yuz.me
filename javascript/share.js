2d41661e7b9f2043bc47ce520efa6e053d3f194d
Zach YU <git@yuz.me>
2014-12-03 00:55:17 +0100

    updates

---
+++ javascript/share.js
@@ -0,0 +1,24 @@
+(function() {
+    var google = "https://script.google.com/macros/s/AKfycbxTFXewV2hSdm4LyOCwQ6XAbjmGPtbCaijU-8Igk1VPmjyHMXs/exec";
+    var Url = google + "?prefix=?";
+
+    $.getJSON(Url).done(function(data) {
+        var content = createOutput(data);
+        var idName = 'share';
+        document.getElementById(idName).innerHTML = content;
+    });
+
+    var createOutput = function( jsonarray ) {
+        var output = [];
+        var array = jsonarray.reverse();
+        var i;
+        for (i = 0; i < array.length; i++) {
+            output.push("<h2 style=\"font-size: 1.7rem;\">" + array[i].Title + "</h2>");
+            output.push("<span class=\"post-date\">" + array[i].Time + "</span>");
+            output.push("<p>" + array[i].Content +"</p>");
+            output.push("<hr>");
+        }
+        var outputString = output.join("");
+        return outputString;
+    };
+})();
--- share.md
+++ share.md
@@ -5,6 +5,8 @@ script: /javascript/share.js
 
 ---
 
+登陆 [http://yuz.me/text](http://yuz.me/text) 即可添加新的内容。新内容会自动显示在下面。
+<hr>
 <div id="share">
     <img src="/public/loading.gif" alt="Loading..." class="loading">
 </div>
