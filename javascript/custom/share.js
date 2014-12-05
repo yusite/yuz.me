a46d9666606e2f3e4590697895af5d88ff83a5a2
Zach YU <git@yuz.me>
2014-12-05 11:24:43 +0100

    add close method

--- javascript/custom/share.js
+++ javascript/custom/share.js
@@ -1,36 +1,49 @@
 (function() {
     var google = "https://script.google.com/macros/s/AKfycbxTFXewV2hSdm4LyOCwQ6XAbjmGPtbCaijU-8Igk1VPmjyHMXs/exec";
     var Url = google + "?prefix=?";
-
-    $.getJSON(Url).done(function(data) {
-        var content = createOutput(data);
-        var idName = 'share';
-        document.getElementById(idName).innerHTML = content;
-    });
-
+    // add http
     function addhttp(url) {
         if (!/^(f|ht)tps?:\/\//i.test(url)) {
             url = "http://" + url;
         }
         return url;
     }
-
+    // output text
+    $.getJSON(Url).done(function(data) {
+        var content = createOutput(data);
+        var idName = 'share';
+        document.getElementById(idName).innerHTML = content;
+    });
     var createOutput = function( jsonarray ) {
         var output = [];
-        var array = jsonarray.reverse();
-        var i,l;
-        for (i = 0; i < array.length; i++) {
-            output.push("<div style=\"padding-top:0.5rem;\">");
-            output.push("<span class=\"post-date\">" + array[i].Time + "</span>");
-            output.push(marked(array[i].Content));
-            if (array[i].Link){
-                l = addhttp(array[i].Link);
-                output.push("<span><a href=\"" + l + "\">" + l + "</a></span>");
+        var i,link;
+        for (i = jsonarray.length - 1; i > -1; i--) {
+            output.push('<div style="padding-top:0.5rem;" id="' + i + '">');
+            output.push("<span class=\"post-date\">");
+            output.push(jsonarray[i].Time);
+            output.push(' <a href="hide" style="float:right;font-size:smaller;">');
+            output.push("Close");
+            output.push("</a>");
+            output.push("</span>");
+            output.push(marked(jsonarray[i].Content));
+            if (jsonarray[i].Link){
+                link = addhttp(jsonarray[i].Link);
+                output.push("<span><a href=\"" + link + "\">" + link + "</a></span>");
             }
-            output.push("</div>");
             output.push("<hr>");
+            output.push("</div>");
         }
         var outputString = output.join("");
         return outputString;
     };
+    // hide text
+    $(document).on("click", 'a[href="hide"]', function( event ){
+        event.preventDefault();
+        var theDiv = $(this).parent().parent();
+        var divID = theDiv.attr('id');
+        var hideID = Number(divID) + 2;
+        var hideUrl = google + "?hide=" + hideID;
+        $.getJSON(hideUrl);
+        theDiv.slideUp();
+    });
 })();
