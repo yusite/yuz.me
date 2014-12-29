0bf1eaaac65a8183e46d8a236658dde7c1644246
Zach YU <git@yuz.me>
2014-12-29 21:51:13 +0100

    update log page

--- javascript/custom/log.js
+++ javascript/custom/log.js
@@ -1,78 +1,43 @@
-(function() {
-    var google = "https://script.google.com/macros/s/AKfycbxRYZQtTQ3qBQtxU5Q1iMV9_hlgvgALyTyID42IUNfDouFsajfN/exec";
+(function(){
+    var google = "https://script.google.com/macros/s/AKfycbz6oLGBd7N1rNmxcOzClQ0SWHyAd8z37bXTeAy9UkmMmj6MzD4/exec";
     var displayUrl = google + "?display=?";
-    // output text
+
+    var outputDoing = function( jsonarray ) {
+        var i, output = [];
+        for (i = 0; i < jsonarray.length; i++) {
+            output.push(jsonarray[i]);
+            output.push('<br>');
+            output.push('<textarea name="summary" rows="1" style="font-size:16px;"></textarea>');
+            output.push('<br>');
+        }
+        var outputString = output.join("");
+        return outputString;
+    };
+
+    var outputLocation = function( value ) {
+        return value;
+    };
+
     $.getJSON(displayUrl).done(function(data) {
-        var content = createDoing(data.doing);
+        var content = outputDoing(data.doing);
         var idName = 'log';
-        document.getElementById(idName).innerHTML = content;
-        content = createSelect(data.recent, 'create', 'Recent Tasks');
-        idName = 'recent';
-        document.getElementById(idName).innerHTML = content;
-        content = createSelect(data.todo, 'todo', 'ToDo List');
-        idName = 'todo';
         document.getElementById(idName).innerHTML = content;
         content = outputLocation(data.place);
         idName = 'place';
         document.getElementById(idName).innerHTML = content;
     });
-    // create doing output
-    var createDoing = function( jsonarray ) {
-        var i, output = [];
-        for (i = 0; i < jsonarray.length; i++) {
-            output.push('<input type="checkbox" name="checked" value="');
-            output.push(jsonarray[i]);
-            output.push('" id="' + i + '">');
-            output.push(' <label for="' + i + '">');
-            output.push(jsonarray[i]);
-            output.push('</label>');
-            output.push('<br>');
-            output.push('<textarea name="post" rows="1" style="font-size:16px;"></textarea>');
-            output.push('<br>');
-        }
-        var outputString = output.join("");
-        return outputString;
-    };
-    // create recent output
-    var createSelect = function( jsonarray, name, option ) {
-        var i, output = [];
-        output.push('<option selected value="">');
-        output.push(option);
-        output.push('</option>');
-        for (i = 0; i < jsonarray.length; i++) {
-            output.push('<option value="');
-            output.push(jsonarray[i]);
-            output.push('">');
-            output.push(jsonarray[i]);
-            output.push('</option>');
-        }
-        var outputString = output.join("");
-        return outputString;
-    };
-    // create location output
-    var outputLocation = function( jsonarray ) {
-        return jsonarray[0];
-    };
-    // validate Form
-    $("#submit").on("click", function(event) {
-        var items = $("form").serialize().split(/=|&/);
-        var count, creates = [], todos = [];
-        for (count = 0; count < items.length; count = count + 2) {
-            var id = count + 1;
-            if (items[count] == 'create') {
-                creates.push(items[id]);
-            } else if (items[count] == 'todo') {
-                todos.push(items[id]);
-            }
-        }
-        var create = creates.join("");
-        var todo = todos.join("");
 
-        if (todo && !create) {
-            $("span#test2").text("Please insert a new task!").show().fadeOut(2000);
-            return false;
-        } else {
-            return true;
-        }
+    $(document).on("click", '#submit', function(event){
+        event.preventDefault();
+        var command = [];
+        command.push(google);
+        command.push('?');
+        command.push($('#form').serialize());
+        command.push('&state=?');
+        $('#send').html('Submitting...');
+        $.getJSON(command.join('')).done(function(data) {
+            window.close();
+        });
     });
+
 })();
--- log.md
+++ log.md
@@ -10,32 +10,23 @@ Pushing forward!
 
 ---
 
-<form action="https://script.google.com/macros/s/AKfycbxRYZQtTQ3qBQtxU5Q1iMV9_hlgvgALyTyID42IUNfDouFsajfN/exec" method="GET">
+<form id="form" method="GET" action="#">
 
 <h3><strong>1. 执行中</strong></h3>
 <div id="log" style="font-size:20px;">...</div>
 
+<hr>
+
 <h3><strong>2. 新建</strong> in
-<a href="https://docs.google.com/a/yuz.me/spreadsheets/d/1k6HhhgqxFaCh5VRzfqmkuODzh59lUI7TUpEEzhWUsLw/edit#gid=669333296" id="place">...</a> <span style="color:red;" id="test2"></span>
-<br>
-<select name="create" id="recent" style="font-size:18px;">
-<option selected value="">Loading...</option>
-</select>
+<a href="https://docs.google.com/a/yuz.me/spreadsheets/d/1Ls3l7bvsyFznq73eSwJL7tkeAbV0PalBBnX0gQFn5DU/edit#gid=2010321559" id="place">...</a>
 <br>
 <input type="text" name="create" style="font-size:18px;">
 </h3>
 
-<h3><strong>3. 待办事项</strong>
-<br>
-<select name="todo" id="todo" style="font-size:18px;">
-<option selected value="">Loading...</option>
-</select> <a href="http://yuz.me/task/">+</a>
-</h3>
-
 <hr>
 
-<p>
-<input type="submit" value="Submit" id="submit" style="font-size:18px;float:right;margin-bottom:60px;">
+<p id="send" style="float:right;">
+<input type="submit" value="Submit" id="submit" style="font-size:18px;">
 </p>
 
 </form>
