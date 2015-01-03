56001bbf5f7d831d4e0348bc480fac2b0a0c3b17
Zach YU <git@yuz.me>
2015-01-03 18:05:11 +0100

    update log page

--- javascript/custom/log.js
+++ javascript/custom/log.js
@@ -9,7 +9,7 @@
             output.push('<span class="taskName">');
             output.push(jsonarray[i]);
             output.push('</span> ');
-            output.push('<button class="done" type="button" style="font-size:18px;border:0;background:none;color:#268bd2;">DONE</button>');
+            output.push('<button class="done" type="button" style="font-size:18px;border:0;background:none;color:#268bd2;padding:0;">DONE</button> ');
             output.push('<button class="repeat" type="button" style="font-size:18px;border:0;background:none;color:#268bd2;padding:0;">↺</button>');
             output.push('<br>');
             output.push('<textarea name="summary" rows="1" style="font-size:18px;"></textarea>');
