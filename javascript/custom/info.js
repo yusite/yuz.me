e2b8288c75b12a180afc7927201081448e127bfe
Zach YU <git@yuz.me>
2015-02-16 10:39:09 +0800

    update info.js

--- javascript/custom/info.js
+++ javascript/custom/info.js
@@ -1,21 +1,39 @@
 (function() {
-    var google = "https://script.google.com/macros/s/AKfycbx9NTBoP4gRf9jmnb89UV5XFVBckW9VwOcLvu-wpCSkdeWKUpY/exec?";
-    var option = "forms&";
-    var prefix = "prefix=?";
-    var Url = google + option + prefix;
-    // output text
-    $.getJSON(Url).done(function(data) {
+/*  var google = "https://script.google.com/macros/s/AKfycbx9NTBoP4gRf9jmnb89UV5XFVBckW9VwOcLvu-wpCSkdeWKUpY/exec?";
+    var option = "forms&sheets";
+    var prefix = "&prefix=?";
+    var Url = google + option + prefix; */
+
+    var Url = "../../i/info.json?callback=?";
+
+    $.ajax({
+        type: "GET",
+        url: Url,
+        jsonpCallback: "output",
+        contentType: "application/json",
+        dataType: 'jsonp',
+    }).done(function(data) {
         var content = createOutput(data);
         var idName = 'info';
         document.getElementById(idName).innerHTML = content;
     });
+
     var createOutput = function( object ) {
         var output = [];
+        var array=[], i;
         if (object.Forms) {
-            var array = object.Forms;
-            output.push('<h1>Forms</h1>');
+            array = object.Forms;
+            output.push('<h2>添加</h2>');
             output.push('<ul>');
-            var i;
+            for (i=0;i<array.length;i++) {
+                output.push('<li><a href="' + array[i].published + '">' + array[i].name + '</a></li>');
+            }
+            output.push('</ul>');
+        }
+        if (object.Sheets) {
+            array = object.Sheets;
+            output.push('<h2>显示</h2>');
+            output.push('<ul>');
             for (i=0;i<array.length;i++) {
                 output.push('<li><a href="' + array[i].published + '">' + array[i].name + '</a></li>');
             }
